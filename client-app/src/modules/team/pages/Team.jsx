2e83a6ea3fa3e086fdbc299c26e0e774aca878d4
Yustomper <157041688+Yustomper@users.noreply.github.com>
2024-12-11 16:38:09 -0300

    Update Team.jsx

--- client-app/src/modules/team/pages/Team.jsx
+++ client-app/src/modules/team/pages/Team.jsx
@@ -1,5 +1,3 @@
-// client-app/src/modules/team/pages/Team.jsx
-
 import React from 'react';
 import { useTheme } from '../../../context/ThemeContext';
 import { useTeams } from '../hooks/useTeams';
@@ -24,17 +22,18 @@ export default function Team() {
   return (
     <div className={`min-h-screen ${isDarkMode ? 'bg-gray-900 text-gray-100' : 'bg-gray-100 text-gray-900'}`}>
       <div className="container mx-auto px-4 py-8">
-        <div className="flex justify-between items-center mb-8">
-          <h1 className={`text-4xl font-bold ${isDarkMode ? 'text-purple-400' : 'text-orange-600'}`}>
+        <div className="flex flex-col md:flex-row justify-between items-center mb-8 gap-4">
+          <h1 className={`text-4xl font-bold ${isDarkMode ? 'text-purple-400' : 'text-orange-600'} mb-4 md:mb-0`}>
             Gestión de Equipos
           </h1>
-          <button
-            onClick={openModal}
-            className={`${isDarkMode ? 'bg-purple-600 hover:bg-purple-700' : 'bg-orange-500 hover:bg-orange-600'} text-white px-6 py-3 rounded-full transition duration-300 flex items-center`}
-          >
-            <PlusIcon className="w-5 h-5 mr-2" />
-            Crear Equipo
-          </button>
+        <button
+          onClick={openModal}
+          className={`${isDarkMode ? 'bg-purple-600 hover:bg-purple-700' : 'bg-orange-500 hover:bg-orange-600'} text-white px-6 py-3 rounded-full transition duration-300 flex items-center justify-center  md:w-auto`}
+        >
+          <PlusIcon className="w-5 h-5 mr-2" />
+          Crear Equipo
+        </button>
+
         </div>
 
         <div className="flex flex-col md:flex-row justify-between items-center mb-8 gap-4">
@@ -47,7 +46,7 @@ export default function Team() {
           />
           <button
             onClick={handleOrderBy}
-            className={`${isDarkMode ? 'bg-gray-700 text-white' : 'bg-white text-black'} 
+            className={`${isDarkMode ? 'bg-gray-700 text-white' : 'bg-white text-black'}
               px-4 py-2 rounded border focus:outline-none focus:ring-2 focus:ring-orange-500`}
           >
             {orderBy === 'name' ? 'Ordenar por nombre' : 'Ordenar por fecha'}: {sortOrder === 'asc' ? 'Ascendente' : 'Descendente'}
